65e9c19c340861ff6bac45e131970950c96984f4
rizkiromadoni <rsahroromadoni@gmail.com>
2024-09-10 09:08:33 +0700

    feat: dotenv load

--- app/server.go
+++ app/server.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
@@ -14,8 +16,14 @@ type Server struct {
 	Router *mux.Router
 }
 
-func (server *Server) Initialize() {
-	fmt.Println("Hello World")
+type AppConfig struct {
+	AppName string
+	AppEnv  string
+	AppPort string
+}
+
+func (server *Server) Initialize(appConfig AppConfig) {
+	fmt.Printf("Running %s \n", appConfig.AppName)
 
 	server.Router = mux.NewRouter()
 	server.InitializeRoutes()
@@ -26,9 +34,27 @@ func (server *Server) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, server.Router))
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
+
 func Run() {
 	server := Server{}
+	appConfig := AppConfig{}
 
-	server.Initialize()
-	server.Run(":3000")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error on loading .env file")
+	}
+
+	appConfig.AppName = getEnv("APP_NAME", "GoShop")
+	appConfig.AppEnv = getEnv("APP_ENV", "development")
+	appConfig.AppPort = getEnv("APP_PORT", "3000")
+
+	server.Initialize(appConfig)
+	server.Run(":" + appConfig.AppPort)
 }
