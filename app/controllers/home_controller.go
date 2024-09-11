5e907fa074a4d17d655c0d132881552a3aaecf6c
rizkiromadoni <rsahroromadoni@gmail.com>
2024-09-11 21:15:36 +0700

    feat: templating and css binding

--- app/controllers/home_controller.go
+++ app/controllers/home_controller.go
@@ -1,10 +1,16 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/unrolled/render"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	rndr := render.New(render.Options{Layout: "layout"})
+
+	rndr.HTML(w, http.StatusOK, "home", map[string]interface{}{
+		"title":       "Home",
+		"description": "Home Description",
+	})
 }
--- app/routes.go
+++ app/routes.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/rizkiromadoni/go-shop/app/controllers"
 )
@@ -8,4 +10,8 @@ import (
 func (server *Server) InitializeRoutes() {
 	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
+
+	staticFileDir := http.Dir("./assets/")
+	staticFileHandler := http.StripPrefix("/public/", http.FileServer(staticFileDir))
+	server.Router.PathPrefix("/public/").Handler(staticFileHandler).Methods("GET")
 }
